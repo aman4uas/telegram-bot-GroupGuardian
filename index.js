76950c971ed0799b29e9beac89727c22564d9e49
aman4uas <aman4u.as@gmail.com>
2023-09-27 13:47:03 +0530

    Update server

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 require("dotenv").config();
+
+const express = require("express");
+const path = require("path");
 const TelegramBot = require("node-telegram-bot-api");
 
+/* Initialise Bot */
 const bot = new TelegramBot(process.env.BOT_TOKEN, { polling: true });
 
+/* Bot Business Logics */
 bot.onText(/\/echo (.+)/, (msg, match) => {
   const chatId = msg.chat.id;
   const resp = match[1];
@@ -11,5 +16,12 @@ bot.onText(/\/echo (.+)/, (msg, match) => {
 
 bot.on("message", (msg) => {
   const chatId = msg.chat.id;
-  bot.sendMessage(chatId, "Received yr message");
+  bot.sendMessage(chatId, "Received ur message");
 });
+
+/* Express Server */
+const app = express();
+app.use(express.static(path.join(__dirname, "public")));
+app.get("/", (req, res) => res.sendFile("index.html"));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
