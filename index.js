8d5894d0f4fe71ab3cd93c35dd8414c1558434e6
aman4uas <aman4u.as@gmail.com>
2023-09-27 12:24:50 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+require("dotenv").config();
+const TelegramBot = require("node-telegram-bot-api");
+
+const bot = new TelegramBot(process.env.BOT_TOKEN, { polling: true });
+
+bot.onText(/\/echo (.+)/, (msg, match) => {
+  const chatId = msg.chat.id;
+  const resp = match[1];
+  bot.sendMessage(chatId, resp);
+});
+
+bot.on("message", (msg) => {
+  const chatId = msg.chat.id;
+  bot.sendMessage(chatId, "Received yr message");
+});
