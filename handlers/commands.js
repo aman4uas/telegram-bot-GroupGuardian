6e25301c84b71569785881e882c1930773b6248e
aman4uas <aman4u.as@gmail.com>
2023-09-29 02:00:52 +0530

    update

--- handlers/commands.js
+++ handlers/commands.js
@@ -1,5 +1,4 @@
 exports.startHandler = async (bot, msg) => {
-  if (msg.chat.type !== "private") return;
   const chatId = msg.chat.id;
   const message =
     "Hi there!\n\nI'm your all-in-one chat group moderator. I filter out URLs, extend a warm welcome to new members, and lend a hand to administrators. My mission is to maintain a safe environment, boost engagement, and lighten the load for moderators. With 24/7 availability, I'm your group's steadfast guardian.";
--- handlers/welcome.js
+++ handlers/welcome.js
@@ -2,7 +2,10 @@ exports.welcomeHandler = (bot, msg) => {
   let chatId = msg.chat.id;
   let newMembers = msg.new_chat_members;
   newMembers.forEach((newMember) => {
-    let username = newMember.first_name + " " + newMember.last_name;
+    let username;
+    if (newMember.last_name)
+      username = newMember.first_name + " " + newMember.last_name;
+    else username = newMember.first_name;
     let message = `Welcome, ${username}! \n\nFeel free to introduce yourself and read the group guidelines/rules.`;
     bot
       .sendMessage(chatId, message)
